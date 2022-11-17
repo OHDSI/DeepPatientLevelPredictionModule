aced6b9d4bf060d1a19de0b6e9ac0a5a66b9f649
jreps <jreps@its.jnj.com>
2022-11-17 11:24:40 -0500

    fixing renv bug
    fixing renv missing ,

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -8,7 +8,7 @@ createDeepPatientLevelPredictionModuleSpecifications <- function(
   
   specifications <- list(
     module = "DeepPatientLevelPredictionModule",
-    version = "0.0.1",
+    version = "0.0.2",
     remoteRepo = "github.com",
     remoteUsername = "ohdsi",
     settings = modelDesignList
