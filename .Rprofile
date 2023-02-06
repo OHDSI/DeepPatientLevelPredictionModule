606710adc96c12f55a5bd2937e3ddf4600ddf56d
egillax <egillax@gmail.com>
2023-02-06 11:40:07 +0100

    add tests and automation

---
+++ .Rprofile
@@ -0,0 +1,2 @@
+source("renv/activate.R")
+Sys.setenv("torch_install" = "1")
---
+++ tests/test-eunomia.R
@@ -0,0 +1,24 @@
+library(testthat)
+library(Eunomia)
+connectionDetails <- getEunomiaConnectionDetails()
+Eunomia::createCohorts(connectionDetails = connectionDetails)
+
+workFolder <- tempfile("work")
+dir.create(workFolder)
+resultsfolder <- tempfile("results")
+dir.create(resultsfolder)
+jobContext <- readRDS("tests/testJobContext.rds")
+jobContext$moduleExecutionSettings$workSubFolder <- workFolder
+jobContext$moduleExecutionSettings$resultsSubFolder <- resultsfolder
+jobContext$moduleExecutionSettings$connectionDetails <- connectionDetails
+
+test_that("Run module", {
+  source("Main.R")
+  execute(jobContext)
+  resultsFiles <- list.files(resultsfolder)
+  expect_true("results.zip" %in% resultsFiles)
+})
+
+unlink(workFolder)
+unlink(resultsfolder)
+unlink(connectionDetails$server())
