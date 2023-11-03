fcc61c1fb5e0f4cdeeab979039e3762796904985
egillax <egillax@gmail.com>
2023-11-03 13:57:30 +0100

    update deepPLP version

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -18,7 +18,7 @@
 createDeepPatientLevelPredictionModuleSpecifications <- function(modelDesignList) {
   specifications <- list(
     module = "DeepPatientLevelPredictionModule",
-    version = "0.1.0",
+    version = "0.2.0",
     remoteRepo = "github.com",
     remoteUsername = "ohdsi",
     settings = modelDesignList
