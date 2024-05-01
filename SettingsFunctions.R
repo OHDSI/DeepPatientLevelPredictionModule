9cedf5d0142ffbf645faa60b60d100d5f9d8d605
egillax <egillax@gmail.com>
2024-05-01 11:43:49 +0200

    fix module

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -18,7 +18,7 @@
 createDeepPatientLevelPredictionModuleSpecifications <- function(modelDesignList) {
   specifications <- list(
     module = "DeepPatientLevelPredictionModule",
-    version = "0.2.2",
+    version = "0.2.3",
     remoteRepo = "github.com",
     remoteUsername = "ohdsi",
     settings = modelDesignList
